floatRand/PoschengbandR

De-randomize starting object quantities.
So that startscumming for an extra potion of speed or two and such isn't
a thing.

src/cavalry.c
@@ -99,7 +99,7 @@ static void _birth(void)
     py_birth_obj_aux(TV_POLEARM, SV_BROAD_SPEAR, 1);
     py_birth_obj_aux(TV_SOFT_ARMOR, SV_LEATHER_SCALE_MAIL, 1);
     py_birth_obj_aux(TV_BOW, SV_SHORT_BOW, 1);
-    py_birth_obj_aux(TV_ARROW, SV_AMMO_NORMAL, rand_range(15, 25));
+    py_birth_obj_aux(TV_ARROW, SV_AMMO_NORMAL, 20);
 }
 
 class_t *cavalry_get_class(void)

src/ranger.c
@@ -46,7 +46,7 @@ static void _birth(void)
     py_birth_obj_aux(TV_SWORD, SV_DAGGER, 1);
     py_birth_obj_aux(TV_SOFT_ARMOR, SV_SOFT_LEATHER_ARMOR, 1);
     py_birth_obj_aux(TV_BOW, SV_SHORT_BOW, 1);
-    py_birth_obj_aux(TV_ARROW, SV_AMMO_NORMAL, rand_range(20, 40));
+    py_birth_obj_aux(TV_ARROW, SV_AMMO_NORMAL, 30);
     py_birth_spellbooks();
 }
 

src/sniper.c
@@ -517,7 +517,7 @@ static void _birth(void)
     py_birth_obj_aux(TV_SWORD, SV_DAGGER, 1);
     py_birth_obj_aux(TV_SOFT_ARMOR, SV_SOFT_LEATHER_ARMOR, 1);
     py_birth_obj_aux(TV_BOW, SV_LIGHT_XBOW, 1);
-    py_birth_obj_aux(TV_BOLT, SV_AMMO_NORMAL, rand_range(20, 30));
+    py_birth_obj_aux(TV_BOLT, SV_AMMO_NORMAL, 25);
 }
 
 class_t *sniper_get_class(void)

nikheizen/poschengband

(cherry picked from commit 5ce8ea8e779a608dd285383a9bed10cc33025a65)
Conflicts:
	src/archer.c
	src/cavalry.c
	src/demigod.c
	src/ranger.c
	src/scout.c
	src/sniper.c
	src/tourist.c
	src/warrior.c
	src/weaponmaster.c
	src/weaponsmith.c

src/cavalry.c
@@ -120,7 +120,7 @@ static void _birth(void)
     py_birth_obj_aux(TV_POLEARM, SV_BROAD_SPEAR, 1);
     py_birth_obj_aux(TV_SOFT_ARMOR, SV_LEATHER_SCALE_MAIL, 1);
     py_birth_obj_aux(TV_BOW, SV_SHORT_BOW, 1);
-    py_birth_obj_aux(TV_ARROW, SV_ARROW, rand_range(15, 25));
+    py_birth_obj_aux(TV_ARROW, SV_ARROW, 20);
 }
 
 class_t *cavalry_get_class(void)

src/ranger.c
@@ -43,7 +43,7 @@ static void _birth(void)
     py_birth_obj_aux(TV_SWORD, SV_DAGGER, 1);
     py_birth_obj_aux(TV_SOFT_ARMOR, SV_SOFT_LEATHER_ARMOR, 1);
     py_birth_obj_aux(TV_BOW, SV_SHORT_BOW, 1);
-    py_birth_obj_aux(TV_ARROW, SV_ARROW, rand_range(20, 40));
+    py_birth_obj_aux(TV_ARROW, SV_ARROW, 30);
     py_birth_spellbooks();
 }
 

src/sniper.c
@@ -5,7 +5,7 @@ static void _birth(void)
     py_birth_obj_aux(TV_SWORD, SV_DAGGER, 1);
     py_birth_obj_aux(TV_SOFT_ARMOR, SV_SOFT_LEATHER_ARMOR, 1);
     py_birth_obj_aux(TV_BOW, SV_LIGHT_XBOW, 1);
-    py_birth_obj_aux(TV_BOLT, SV_BOLT, rand_range(20, 30));
+    py_birth_obj_aux(TV_BOLT, SV_BOLT, 25);
 }
 
 /************************************************************************